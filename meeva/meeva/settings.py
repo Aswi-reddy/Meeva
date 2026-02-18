dcb5dc43b7a18f12ddcb5c7ec97f311dc9d49e2b
Nithin Nalluri <nithintony361@gmail.com>
2026-02-19 01:50:24 +0530

    Added landing page and configured SQLite database

--- meeva/meeva/settings.py
+++ meeva/meeva/settings.py
@@ -78,15 +78,12 @@ WSGI_APPLICATION = 'meeva.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "meevadb",
-        "USER": "suma",
-        "PASSWORD": "suma@123",
-        "HOST": "10.218.186.177",
-        "PORT": "3308",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
